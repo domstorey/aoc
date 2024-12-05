e42bc87270462d0a239f3c73eba03da8e5870914
Dominic Storey <domstorey@uk.ibm.com>
2024-12-05 10:19:36 +0000

    day1 start

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aoc
+
+### init
+go init mod
+
+### day1 run
+ go run day1/day1.go
--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,47 @@
+// Assisted by watsonx Code Assistant 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	file, err := os.Open("day1/input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "   ")
+		if len(parts) != 2 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		num1, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error converting first number:", err)
+			continue
+		}
+
+		num2, err := strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Println("Error converting second number:", err)
+			continue
+		}
+
+		fmt.Println(num1, num2)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
 }
